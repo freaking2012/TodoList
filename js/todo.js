ad5cd62666c49500e854da23467119ef8ab00a40
Manu Mishra <mishra.manu0@gmail.com>
2015-06-22 01:57:04 +0530

    TodoList Files
    todo list files

---
+++ js/todo.js
@@ -0,0 +1,101 @@
+//Angular JS starts
+
+function TodoCtrl($scope)
+{
+	$scope.todos=[];
+	
+	$('#myModal').css({'position':'fixed','height':'100%','top': '30%'});
+		
+		if(localStorage.length!="undefined")
+	{
+		//alert("length: "+localStorage.length);
+		for(var i=0;i<localStorage.length;i++)
+		{
+			var task = localStorage.getItem(localStorage.key(i));
+			if(task.split('|')[1]=='true')
+			$scope.todos.push({id:localStorage.key(i),text:task.split('|')[0],done:true});			
+		else
+			$scope.todos.push({id:localStorage.key(i),text:task.split('|')[0],done:false});
+			//alert(task.split('|')[1]);
+			//alert(localStorage.key(i));
+		}
+		$scope.TodoText='';
+		
+	}
+	else{
+			$scope.todos = [{text:"Add ToDos to this list",done:false},
+					{text:"Clear all the ToDos after completing them",done:false}];
+	}	
+	
+	
+		
+		
+
+	
+	
+	
+	
+	
+	
+	$scope.clearCompleted = function(){
+		
+			//localStorage.clear();
+		
+		var count=0;
+		$scope.todos = _.filter($scope.todos,function(todo){
+			if(todo.done==true)			
+			{				
+				//document.writeln(todo.id);
+				//alert(todo.id);
+				localStorage.removeItem(todo.id);
+				count++;
+			
+			}
+			//alert(todo.id+todo.done);
+			
+			
+			return !todo.done;	
+				
+		})
+		if(count==0)
+			{
+				$("#popUpMsg").text("No item selected. No item removed. Tit for Tat !!");
+				$("#myModal").modal();				
+			}
+		//document.write($scope.todos);
+	}
+	
+	$scope.addTodo = function(){
+		
+		if($scope.TodoText=='')
+		{
+			$("#popUpMsg").text("Hello!! Please enter a To Do Item First.");
+			$("#myModal").modal();
+			return;
+		}
+		
+		var nextId = localStorage.length+1;
+		$scope.todos.push({id:"todo"+nextId,text:$scope.TodoText,done:false});
+		
+		if (typeof(Storage) != "undefined") {
+			
+			localStorage.setItem("todo"+nextId,$scope.TodoText+"|"+false);
+		}
+		$scope.TodoText='';
+	}
+	$scope.stateChanged = function(chk)
+	{
+		//document.write(chk.id);
+		var itemId = chk.id;
+		//alert(itemId);
+		localStorage.setItem(chk.id,localStorage.getItem(chk.id).split('|')[0]+"|"+chk.done);
+		
+	}
+	
+	
+	
+	
+}
+
+
+
